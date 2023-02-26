884e07b2a3d5c21e0c690fb519773491e77d68d6
moncef aissaoui <108152453+modecode22@users.noreply.github.com>
2023-02-26 23:39:07 +0100

    alote

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,15 @@ module.exports = {
 
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
  theme: {
+  colors:{
+        w1:"#ECF2FF",
+    w2:"#E3DFFD",
+    w3:"#E5D1FA",
+    d1:"#1A1A2E",
+    d2:"#16213E",
+    d3:"#0F3460",
+    d4:"#E94560",
+  }
   },
 	plugins: [],
 }
